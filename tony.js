f4e78adf1d3dbb18c421ed3b911545b658f6c84b
tvander5 <tvander5@email.cpcc.edu>
2022-11-08 12:02:30 -0500

    Update tony.js

--- tony.js
+++ tony.js
@@ -1,38 +1,62 @@
+// The grid div
 let allButtons = document.getElementById("buttons")
-let amountOfChosenNumbers = 10
+
+// The amount of grid spots
+let gridSpots = 25
+
+// The amount of numbers to find
+let amountOfChosenNumbers = 2
+
+// Numbers to find
 let chosenNumbers = []
 
+// Generate a random number from 0 to y
 let RandomRange = (y) => {
     return Math.floor(Math.random() * y);
 }
 
+// Check if value exists in an array
 let ValueInArray = (v, arr) => {
+    // Set the return value
     let ret = false
     arr.forEach(x => {
         if (x == v) {
-            console.log(v, arr)
-            ret = true;
+            ret = true; // Exit the loop
         }
     });
 
     return ret;
 }
 
-let GenerateButtons = (buttonCount) => {
-    chosenNumbers = []
+// Generate the random number
+let GenerateNumbers = () => {
+    chosenNumbers = [] // clear the array
+
+    // Loop through each number slot
     for (let index = 0; index < amountOfChosenNumbers; index++) {
         let done = false
+
+        // Generate a random number
         while (!done) {
             let val = RandomRange(buttonCount)
+            // Check if number has already been generated
             if (!ValueInArray(val, chosenNumbers)) {
+                // Add number and exit loop
                 chosenNumbers.push(val)
                 done = true
             }
         }
     }
+}
 
+// Generate the button div's
+let GenerateButtons = (buttonCount) => {
+    // Loop through each button slot
     for (let i = 0; i < buttonCount; i++) {
+        // Check if number is a chosen number
         let chosenButton = ValueInArray(i, chosenNumbers)
+
+        // Set the class depending on if the button is chosen
         if (chosenButton) {
             allButtons.innerHTML += "<button class= \"correct-tile\">_</button>"
         } else {
@@ -41,4 +65,6 @@ let GenerateButtons = (buttonCount) => {
     }
 }
 
-GenerateButtons(60)
+// Start the game
+GenerateNumbers()
+GenerateButtons(gridSpots)
