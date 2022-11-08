5e3e3a8a43e8ca57323424cbfbd8863ec1881cf9
Lucas <lprieto0@email.cpcc.edu>
2022-11-08 10:59:35 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+ 1. App should use JavaScript, HTML, and CSS
+2. The page should display a grid of buttons containing numbers from 1-25
+3. The initial class name of any button containing numbers must be “tile” with a hex
+code of rgb(27, 62, 119) and this color may NOT be used anywhere else in the
+exercise.
+4. Numbers should have a randomized order
+5. When a user clicks a number, if it is the next number in order, then its background
+turns a different color for the remainder of the test. If it is the wrong number,
+nothing happens
+6. Once all numbers have been clicked in order, display a win message with the text
+“You Win!”.
+7. Include a button to start a new game at any time with the text content “Start!”.
+8. Hitting the “Start!” button defaults to starting a game containing 25 numbers and
+clicking the start button on subsequent clicks defaults to starting a game
+containing 25 numbers and will shuffle the numbers and restart the game. No
+other buttons to start a game are allowed.
+*/ 
+
+//need to make a loop adding numbers into an array up to max number 
+//should be like while i<max array.push i
+//rng to select a number from the array that is UNIQUE
+//maybe just need to guarantee the array value is unique? so if array[random] = array[random] then jump to top
