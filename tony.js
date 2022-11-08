0bfec15ae4ef5aa570e762266d2867b7541f3a8c
tvander5 <tvander5@email.cpcc.edu>
2022-11-08 11:15:37 -0500

    updated tony.js

---
+++ tony.js
@@ -0,0 +1,18 @@
+let allButtons = document.getElementById("buttons")
+
+let RandomRange = (y) => {
+    return Math.floor(Math.random() * y);
+}
+
+let GenerateButtons = (buttonCount) => {
+    let chosenButton = RandomRange(buttonCount+1)
+    for (let index = 0; index < buttonCount; index++) {
+        if (chosenButton == index) {
+            allButtons.innerHTML += "<button class= \"correct-tile\">_</button>"
+        } else {
+            allButtons.innerHTML += "<button class= \"tile\">_</button>"
+        }
+    }
+}
+
+GenerateButtons(25)
